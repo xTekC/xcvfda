91d323f54a36c513ea378455072f6d3bb02186cc
xTekC <xtekc.dev@pm.me>
2023-09-11 02:09:44 -0400

    update btn bg

--- src/content.rs
+++ src/content.rs
@@ -14,7 +14,7 @@ pub fn render_content<'a>(cx: &Scope<'a>) -> Element<'a> {
     cx.render(rsx! {
         div {
             class: "has-background-black has-text-white is-flex is-flex-direction-column is-align-items-center is-justify-content-center",
-            style: "height: calc(100vh - 100px); overflow: hidden; background-color: #000000;",
+            style: "height: calc(100vh - 100px); overflow: hidden;",
 
             div {
                 class: "has-background-black is-flex is-justify-content-center is-align-items-center",
@@ -31,7 +31,7 @@ pub fn render_content<'a>(cx: &Scope<'a>) -> Element<'a> {
                 style: "height: 70px;",  
                 button {
                     onclick: on_switch_click,
-                    class: "button is-black",
+                    //class: "button is-black",
                     style: "width: 80px; height: 60px; margin: 5px 0;",  
                     img {
                         src: "/flip.png",
