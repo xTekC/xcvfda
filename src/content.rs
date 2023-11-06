905eb180175fd781d33074898480c594f72cdf45
xTekC <xtekc.dev@pm.me>
2023-11-06 14:08:03 -0500

    update

--- src/content.rs
+++ src/content.rs
@@ -1,16 +1,14 @@
 use dioxus::prelude::*;
 
 pub fn render_content<'a>(cx: &Scope<'a>) -> Element<'a> {
-    cx.render(rsx! {
-        div {
-            div {
-                style: "width: 99%; height: 90%; margin: auto;",
-                video {
-                    id: "cameraVideo",
-                    style: "width: 100%; height: 100%; object-fit: fill;",
-                    autoplay: true,
-                }
-            }
+cx.render(rsx! {
+    div {
+        style: "flex: 1; width: 99%; margin: auto; max-height: 100%;", // Ensure the div does not overflow the flex container
+        video {
+            id: "cameraVideo",
+            style: "width: 100%; height: auto; max-height: 100%; object-fit: fill;", // Adjust video sizing to be responsive within the flex item
+            autoplay: true,
         }
-    })  
+    }
+})
 }
