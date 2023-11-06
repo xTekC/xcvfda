f51bea6578e0b75c6adaeaa0630d20165669b440
xTekC <xtekc.dev@pm.me>
2023-11-06 13:57:43 -0500

    update

--- src/content.rs
+++ src/content.rs
@@ -4,7 +4,7 @@ pub fn render_content<'a>(cx: &Scope<'a>) -> Element<'a> {
     cx.render(rsx! {
         div {
             div {
-                style: "width: 99%; height: 90% margin: auto;",
+                style: "width: 99%; height: 90%; margin: auto;",
                 video {
                     id: "cameraVideo",
                     style: "width: 100%; height: 100%; object-fit: fill;",
