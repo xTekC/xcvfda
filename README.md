ad0e1ccc1898b780126ae8fc42ef407f11f4d7e5
xTekC <xtekc.dev@pm.me>
2023-11-06 13:41:25 -0500

    update

--- README.md
+++ README.md
@@ -12,9 +12,6 @@ Cross-platform Computer Vision Face Detection Application
 Attributions
 <br>
 <a href="https://www.flaticon.com/free-icons/vision">created by Assia Benkerroum  - Flaticon</a>
-<br>
-<a href="https://www.flaticon.com/free-animated-icons/refresh">created by Freepik - Flaticon</a>
-<br>
 
 Copyright (c) **xTekC** <br>
 Licensed under [MPL-2.0](LICENSE)
--- src/content.rs
+++ src/content.rs
@@ -7,7 +7,7 @@ pub fn render_content<'a>(cx: &Scope<'a>) -> Element<'a> {
                 style: "width: 99%; margin: auto;",
                 video {
                     id: "cameraVideo",
-                    style: "width: 100%; height: 100%; object-fit: cover;",
+                    style: "width: 100%; height: 100%; object-fit: fill;",
                     autoplay: true,
                 }
             }
